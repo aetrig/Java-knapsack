b86961b5ddab479fc6519957f2907a7c75198e48
aetrig <emilia.gosztyla@gmail.com>
2025-05-28 11:40:16 +0200

    Tests done

--- src/test/java/knapsack/ProblemTest.java
+++ src/test/java/knapsack/ProblemTest.java
@@ -11,4 +11,66 @@ public class ProblemTest {
 		Assert.assertEquals(5, p.items.size());
 	}
 
+	@Test
+	public void atLeastOneTest() {
+		Problem p = new Problem(10, 1, 1, 20);
+		Result result = p.solve(10);
+		boolean isAtLeastOneWithinCapacity = false;
+		for (int i = 0; i < p.itemCount; i++) {
+			if (p.items.get(i).weight <= 10) {
+				isAtLeastOneWithinCapacity = true;
+			}
+		}
+		if (isAtLeastOneWithinCapacity) {
+			Assert.assertTrue(result.items.size() > 0);
+		}
+	}
+
+	@Test
+	public void emptyTest() {
+		Problem p = new Problem(10, 1, 15, 20);
+		int capacity = 10;
+		Result result = p.solve(capacity);
+		boolean isAtLeastOneWithinCapacity = false;
+		for (int i = 0; i < p.itemCount; i++) {
+			if (p.items.get(i).weight <= capacity) {
+				isAtLeastOneWithinCapacity = true;
+			}
+		}
+		if (!isAtLeastOneWithinCapacity) {
+			Assert.assertEquals(0, result.items.size());
+		}
+	}
+
+	@Test
+	public void allInRange() {
+		int lowerBound = 5;
+		int upperBound = 10;
+		Problem p = new Problem(10, 1, lowerBound, upperBound);
+		boolean allInRange = true;
+		for (int i = 0; i < p.itemCount; i++) {
+			int weight = p.items.get(i).weight;
+			int value = p.items.get(i).value;
+			if (weight > upperBound) {
+				allInRange = false;
+			}
+			if (weight < lowerBound) {
+				allInRange = false;
+			}
+			if (value > upperBound) {
+				allInRange = false;
+			}
+			if (value < lowerBound) {
+				allInRange = false;
+			}
+		}
+		Assert.assertTrue(allInRange);
+	}
+
+	@Test
+	public void specificInstance() {
+		Problem p = new Problem(10, 1, 1, 10);
+		Result result = p.solve(15);
+		Assert.assertArrayEquals(new int[] { 15, 33 }, new int[] { result.getWeightSum(), result.getValueSum() });
+	}
 }
